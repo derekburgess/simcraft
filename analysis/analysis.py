a2fb3818a013d8f512c2fd703e8e9a4bf185e6e1
Derek <contact@derekburgess.com>
2024-05-13 19:26:31 -0400

    Updated for consistancy and look

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -21,39 +21,19 @@ color_map = {
 def cluster(df):
     center_x, center_y = 600, 600
     df['distance'] = np.sqrt((df['posx'] - center_x)**2 + (df['posy'] - center_y)**2)
-    fig = plt.figure(figsize=(12, 8))
-    ax = fig.add_subplot(111, projection='3d')
+    figure = plt.figure(figsize=(8, 8))
+    axis = figure.add_subplot(111, projection='3d')
 
     for object_type, color in color_map.items():
         type_df = df[df['type'] == object_type]
-        ax.scatter(type_df['mass'], type_df['distance'], type_df['observation'], c=color, alpha=0.5, label=object_type)
+        axis.scatter(type_df['mass'], type_df['distance'], type_df['observation'], c=color, alpha=0.5, label=object_type)
 
-    ax.set_xlabel('Mass')
-    ax.set_ylabel('Distance from Center')
-    ax.set_zlabel('Observation Year')
-    ax.set_title('Entity Distance from Center over Time')
-
+    axis.set_xlabel('Mass')
+    axis.set_ylabel('Distance from Center', fontsize=10)
+    axis.set_zlabel('Observation Year', fontsize=10)
+    axis.set_title('Entity Distance from Center over Time', fontsize=10)
     plt.legend()
     plt.tight_layout()
-    plt.show()
-
-
-def heatmap(df):
-    aggregated_data = df.groupby('entityid').agg({'posx': 'mean', 'posy': 'mean', 'mass': 'sum', 'flux': 'min', 'type': 'first'}).reset_index()
-    blackholes = aggregated_data[aggregated_data['type'] == 'BlackHole']
-    neutronstars = aggregated_data[aggregated_data['type'] == 'NeutronStar']
-    low_flux_bodies = aggregated_data[(aggregated_data['flux'] < 255) & (aggregated_data['type'] != 'BlackHole') & (aggregated_data['type'] != 'NeutronStar')]
-    other_bodies = aggregated_data[(aggregated_data['flux'] >= 255) & (aggregated_data['type'] != 'BlackHole') & (aggregated_data['type'] != 'NeutronStar')]
-
-    plt.figure(figsize=(12, 8))
-    plt.scatter(other_bodies['posx'], other_bodies['posy'], c='gray', alpha=0.5, zorder=1, s=other_bodies['mass'], label='Molecular Clouds')
-    plt.scatter(low_flux_bodies['posx'], low_flux_bodies['posy'], c='orange', marker='^', alpha=0.5, zorder=2, s=low_flux_bodies['mass'], label='Low Flux Protostars')
-    plt.scatter(blackholes['posx'], blackholes['posy'], c='red', marker='x', s=50, zorder=3, label='Black Holes')
-    plt.scatter(neutronstars['posx'], neutronstars['posy'], c='blue', marker='o', s=25, zorder=4, label='Neutron Stars')
-    plt.title('Event Distribution')
-    plt.xlabel('Position X')
-    plt.ylabel('Position Y')
-    plt.legend()
     plt.show()
 
 
@@ -64,30 +44,54 @@ def timeseries(df):
     for category in grouped.columns:
         ax.plot(grouped.index, grouped[category], label=category, color=color_map[category], marker='o', linestyle='-')
 
-    ax.set_title('Time Series of Astronomical Entities')
-    ax.set_xlabel('Observation Year')
-    ax.set_ylabel('Number of Entities')
-    ax.grid(True)
-    
+    ax.set_title('Time Series of Astronomical Entities', fontsize=10)
+    ax.set_xlabel('Observation Year', fontsize=10)
+    ax.set_ylabel('Number of Entities', fontsize=10)
+    ax.grid(True, linestyle='--', color='#BCBCBC', alpha=0.5, zorder=0)
     plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+
+def heatmap(df):
+    aggregated_data = df.groupby('entityid').agg({'posx': 'mean', 'posy': 'mean', 'mass': 'sum', 'flux': 'min', 'type': 'first'}).reset_index()
+    blackholes = aggregated_data[aggregated_data['type'] == 'BlackHole']
+    neutronstars = aggregated_data[aggregated_data['type'] == 'NeutronStar']
+    variable_flux_entities = aggregated_data[(aggregated_data['flux'] < 255) & (aggregated_data['type'] != 'BlackHole') & (aggregated_data['type'] != 'NeutronStar')]
+    static_entities = aggregated_data[(aggregated_data['flux'] >= 255) & (aggregated_data['type'] != 'BlackHole') & (aggregated_data['type'] != 'NeutronStar')]
+
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(111)
+    ax.set_facecolor('#021F6C')
+
+    plt.scatter(static_entities['posx'], static_entities['posy'], c='#00B86A', s=static_entities['mass'], alpha=0.7, zorder=1, label='Molecular Clouds')
+    plt.scatter(variable_flux_entities['posx'], variable_flux_entities['posy'], c='orange', marker='^', s=50, alpha=0.8, zorder=2, label='Variable Flux Entities')
+    plt.scatter(blackholes['posx'], blackholes['posy'], c='red', marker='o', s=25, alpha=0.8, zorder=5, label='Black Holes')
+    plt.scatter(neutronstars['posx'], neutronstars['posy'], c='yellow', marker='D', s=25, alpha=0.8, zorder=4, label='Neutron Stars')
+    plt.title('Event Distribution', fontsize=10)
+    plt.xlabel('Position X', fontsize=10)
+    plt.ylabel('Position Y', fontsize=10)
+    plt.grid(True, linestyle='--', color='#ffffff', alpha=0.5, zorder=0)
+    plt.legend()
+    plt.tight_layout()
     plt.show()
 
 
 def main():
     parser = argparse.ArgumentParser(description="Run various analysis against the simulation data")
     parser.add_argument("--cluster", action="store_true", help="View a cluste plot of entity Distance from Center")
-    parser.add_argument("--heatmap", action="store_true", help="View a heatmap of Event Distribution")
     parser.add_argument("--time", action="store_true", help="View a time series of Astronomical Entities")
+    parser.add_argument("--heatmap", action="store_true", help="View a heatmap of Event Distribution")
 
     args = parser.parse_args()
 
     if args.cluster:
         cluster(df)
-    if args.heatmap:
-        heatmap(df)
     if args.time:
         timeseries(df)
-
+    if args.heatmap:
+        heatmap(df)
+    
 
 if __name__ == "__main__":
     main()
